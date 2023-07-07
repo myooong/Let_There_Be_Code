8270921bde8805b28f6837fcacd5b0df8e7dc028
myooong <48705990+myooong@users.noreply.github.com>
2023-07-07 11:59:44 +0900

    Vector Container 에서 중복원소 제거한 값을 Return
    Vector Container 에서 중복원소 제거한 값을 Return

---
+++ template_count.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+template<typename T>
+std::vector<T> count(const std::vector<T> & container) {
+    std::vector<T> containerSet;
+
+    for (auto element : container) {
+        auto it = find(containerSet.begin(), containerSet.end(), element);
+        if (it == containerSet.end())
+            containerSet.push_back(element);
+    }
+
+    return containerSet;
+}
+
+
+int main()
+{
+    std::vector<std::string> test_vector = { "kim", "lee", "kim", "kim", "choi", "Hwang"};
+    std::vector<std::string> ret = count(test_vector);
+    for (auto word : ret) {
+        std::cout << word << std::endl;
+    }
+
+}
